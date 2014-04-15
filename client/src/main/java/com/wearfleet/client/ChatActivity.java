6ff1e91aae4c3d7a32034364565dbb450f9ed6a3
Kurt Nelson <kurtisnelson@gmail.com>
2014-04-15 16:51:47 -0400

    Clear chat notification when chat is viewed

--- client/src/main/java/com/wearfleet/client/ChatActivity.java
+++ client/src/main/java/com/wearfleet/client/ChatActivity.java
@@ -1,11 +1,18 @@
 package com.wearfleet.client;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 import com.wearfleet.core.SingleFragmentActivity;
 
 public class ChatActivity extends SingleFragmentActivity {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sendBroadcast(new Intent(FleetService.CLEAR_CHATS_FILTER));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- client/src/main/java/com/wearfleet/client/FleetService.java
+++ client/src/main/java/com/wearfleet/client/FleetService.java
@@ -2,10 +2,13 @@ package com.wearfleet.client;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.text.Html;
 import android.widget.Toast;
 
 import com.pusher.client.connection.ConnectionState;
@@ -16,6 +19,7 @@ import de.greenrobot.event.EventBus;
 
 public class FleetService extends com.wearfleet.core.FleetService {
     private static final String TAG = "FleetService";
+    public static final String CLEAR_CHATS_FILTER = "CLEAR_CHATS_FILTER";
     private final int NOTIFICATION_ID = 0;
     private NotificationManager mNotifyMgr;
     private NotificationCompat.Builder mBuilder;
@@ -33,6 +37,7 @@ public class FleetService extends com.wearfleet.core.FleetService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         setupNotification();
+        registerReceiver(clearChatsReceiver, new IntentFilter(CLEAR_CHATS_FILTER));
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -40,6 +45,7 @@ public class FleetService extends com.wearfleet.core.FleetService {
     public void onDestroy() {
         EventBus.getDefault().unregister(this);
         stopNotification();
+        unregisterReceiver(clearChatsReceiver);
         super.onDestroy();
     }
 
@@ -48,7 +54,7 @@ public class FleetService extends com.wearfleet.core.FleetService {
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_launcher)
                         .setContentTitle(getString(R.string.status_unknown))
-                        .setContentText("No messages");
+                        .setContentText(getString(R.string.no_messages));
 
         mInboxStyle =
                 new NotificationCompat.InboxStyle();
@@ -56,14 +62,22 @@ public class FleetService extends com.wearfleet.core.FleetService {
 
         mBuilder.setStyle(mInboxStyle);
 
-        Intent resultIntent = new Intent(this, ChatActivity.class);
+        Intent chatIntent = new Intent(this, ChatActivity.class);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
         stackBuilder.addParentStack(ChatActivity.class);
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
+        stackBuilder.addNextIntent(chatIntent);
+        PendingIntent chatPendingIntent =
                 stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.addAction(android.R.drawable.sym_action_chat, "Chat", chatPendingIntent);
 
-        mBuilder.setContentIntent(resultPendingIntent);
+        Intent statusIntent = new Intent(this, StatusActivity.class);
+        TaskStackBuilder statusStackBuilder = TaskStackBuilder.create(this);
+        statusStackBuilder.addParentStack(StatusActivity.class);
+        statusStackBuilder.addNextIntent(statusIntent);
+        PendingIntent statusPendingIntent =
+                statusStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(statusPendingIntent);
 
         mBuilder.setOngoing(true);
 
@@ -87,8 +101,8 @@ public class FleetService extends com.wearfleet.core.FleetService {
 
     public void onEventMainThread(ChatEvent e) {
         mBuilder.setNumber(++numMessages);
-        mBuilder.setContentText("New Messages");
-        mInboxStyle.addLine(e.getMessage());
+        mBuilder.setContentText(getString(R.string.new_messages));
+        mInboxStyle.addLine(Html.fromHtml("<strong>"+e.getName()+":</strong> " + e.getMessage()));
         mNotifyMgr.notify(
                 NOTIFICATION_ID,
                 mBuilder.build());
@@ -114,4 +128,20 @@ public class FleetService extends com.wearfleet.core.FleetService {
     public static boolean isRunning(Context c) {
         return com.wearfleet.core.FleetService.isRunning(c, FleetService.class);
     }
+
+    protected BroadcastReceiver clearChatsReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            numMessages = 0;
+            mBuilder.setNumber(0);
+            mBuilder.setContentText(getString(R.string.no_messages));
+            mInboxStyle =
+                    new NotificationCompat.InboxStyle();
+            mBuilder.setStyle(mInboxStyle);
+            mNotifyMgr.notify(
+                    NOTIFICATION_ID,
+                    mBuilder.build());
+            onEventMainThread(EventBus.getDefault().getStickyEvent(ConnectionState.class));
+        }
+    };
 }
