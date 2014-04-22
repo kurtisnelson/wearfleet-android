6553a368a4ea3640636bfb35bb17cff35c8ca349
Kurt Nelson <kurtisnelson@gmail.com>
2014-04-22 16:29:30 -0400

    Fix chat

--- glass/src/main/java/com/wearfleet/glass/ChatActivity.java
+++ glass/src/main/java/com/wearfleet/glass/ChatActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.wearfleet.core.events.ChatEvent;
 
@@ -12,21 +13,21 @@ import java.util.List;
 import de.greenrobot.event.EventBus;
 
 public class ChatActivity extends FragmentActivity{
-    private static final int SPEECH_MESSAGE = 0;
+    private static final int SPEECH_REQUEST = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        startActivityForResult(intent, SPEECH_MESSAGE);
-        finish();
+        startActivityForResult(intent, SPEECH_REQUEST);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == SPEECH_MESSAGE && resultCode == RESULT_OK){
+        if(requestCode == SPEECH_REQUEST && resultCode == RESULT_OK){
             List<String> results = data.getStringArrayListExtra(
                     RecognizerIntent.EXTRA_RESULTS);
             String spokenText = results.get(0);
+            Log.d("Chat", spokenText);
             EventBus.getDefault().post(new ChatEvent(spokenText));
             finish();
         }
